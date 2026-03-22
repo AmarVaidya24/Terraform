74e8c6180092f50cb386bf20a5ad75d4df2d79d3
Amar vaidya <amar.vaidya24@hotmail.com>
2026-03-22 17:52:49 +0530

    added new files

---
+++ Day1/Vnet-udpate.ps1
@@ -0,0 +1,13 @@
+﻿
+$resouceGroup= "NewTerraform"
+$vnetName="VirtualNetwork"
+$newsubnetName="subnet2"
+$newsubnetPrefix ="10.0.1.0/24"
+
+$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resouceGroup
+
+Add-AzVirtualNetworkSubnetConfig -Name $newsubnetName -AddressPrefix $newsubnetPrefix -VirtualNetwork $vnet
+
+$vnet | Set-AzVirtualNetwork
+
+8a566038-6a0a-478b-8291-25d5692f815a
---
+++ Day1/powershellScriptToCreateResource.ps1
@@ -0,0 +1,18 @@
+﻿# https://learn.microsoft.com/en-us/powershell/azure/install-azps-windows?view=azps-15.4.0&tabs=windowspowershell&pivots=windows-psgallery
+# to connect azure account. 
+# Connect-AzAccount -DeviceCode
+
+$resouceGroup= "NewTerraform"
+$location="westeurope"
+$vnetName="VirtualNetwork"
+$subnetName="subnet1"
+$addressPrefix ="10.0.0.0/16"
+$subnetPrefix ="10.0.0.0/24"
+
+New-AzResourceGroup -Name $resouceGroup -Location $location
+
+#Create the subnet config
+$SubnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $addressPrefix
+
+#Create virtual network with subnet
+New-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resouceGroup -Location $location -AddressPrefix $addressPrefix -Subnet $SubnetConfig
